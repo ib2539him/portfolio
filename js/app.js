cfb33cf7eb7e12db4cc8e53d8692417bf13379bb
Md Ibrahim Khalil <129780831+ib2539him@users.noreply.github.com>
2023-04-05 10:17:44 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+
+
+  // one page nav
+  $("#nav").onePageNav({
+    currentClass: "current",
+    changeHash: false,
+    scrollSpeed: 1500,
+    scrollThreshold: 0.5,
+    filter: "",
+    easing: "swing",
+
+  });
+
+
+  // javascript Aos plugin scroll smothly
+  AOS.init();
+})
+
+
+
+
+
